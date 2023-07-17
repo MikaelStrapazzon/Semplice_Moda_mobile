852c118a999a56ae79945a51989cc96c234e2006
mikael <mikael.quadros@gmail.com>
2023-07-17 03:06:41 -0300

    tela de Login atualizada

--- lib/pages/LoginPage/login_page.dart
+++ lib/pages/LoginPage/login_page.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 import 'package:semplice_moda_mobile/pages/DashboardPage/dashboard_page.dart';
 
 class LoginPage extends StatelessWidget {
   LoginPage({super.key});
+
   static String id = '/LoginPage';
 
-  final usernameController = TextEditingController();
-  final passwordController = TextEditingController();
+  final formKey = GlobalKey<FormState>();
+  final TextEditingController nameController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -14,43 +17,105 @@ class LoginPage extends StatelessWidget {
     Color secondaryColor = Theme.of(context).colorScheme.secondaryContainer;
 
     return Scaffold(
-      backgroundColor: Colors.transparent,
-      body: Container(
-        decoration: BoxDecoration(
-          gradient: LinearGradient(
+        backgroundColor: Colors.transparent,
+        body: Container(
+          decoration: BoxDecoration(
+              gradient: LinearGradient(
             colors: [primaryColor, secondaryColor],
             stops: const [0.1, 1],
             begin: Alignment.topLeft,
             end: Alignment.bottomRight,
-          )
+          )),
+          padding: const EdgeInsets.all(16.0),
+          child: Form(
+            key: formKey,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Image(
+                  image: AssetImage('assets/logos/logo.png'),
+                  width: 120,
+                ),
+                const Text(
+                  'Semplice Moda ERP',
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 22,
+                      fontWeight: FontWeight.bold),
+                  textAlign: TextAlign.center,
+                ),
+                const SizedBox(height: 16.0),
+                TextFormField(
+                  controller: nameController,
+                  decoration: const InputDecoration(
+                    labelText: 'Name',
+                    errorStyle: TextStyle(color: Colors.red),
+                  ),
+                  autovalidateMode: AutovalidateMode.onUserInteraction,
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter a name';
+                    }
+                    return null;
+                  },
+                ),
+                const SizedBox(height: 16.0),
+                TextFormField(
+                  controller: passwordController,
+                  obscureText: true,
+                  decoration: const InputDecoration(
+                    labelText: 'Password',
+                    errorStyle: TextStyle(color: Colors.red),
+                  ),
+                  autovalidateMode: AutovalidateMode.onUserInteraction,
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter a password';
+                    }
+                    return null;
+                  },
+                ),
+                const SizedBox(height: 24.0),
+                ElevatedButton(
+                  style: ButtonStyle(
+                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                      RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(8.0),
+                      ),
+                    ),
+                  ),
+                  child: const Text(
+                    'Login',
+                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                  ),
+                  onPressed: () {
+                    if (!formKey.currentState!.validate()) {
+                      return;
+                    }
 
-        ),
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Icon(Icons.adb, size: 80),
-            const Text('SempliceModa ERP'),
-            const TextField(
-              decoration: InputDecoration(
-                labelText: 'Username',
-              ),
+                    http.post(
+                        Uri.parse('http://192.168.0.112/SemplicePDM/login.php'),
+                        body: {
+                          'name': nameController.text,
+                          'password': passwordController.text
+                        }).then((value) {
+                      if (value.body.isNotEmpty) {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => const DashboardPage()));
+                      } else {
+                        ScaffoldMessenger.of(context).showSnackBar(
+                            const SnackBar(
+                                backgroundColor: Colors.red,
+                                content: Text('Name or Password is wrong')));
+                      }
+                    });
+                  },
+                ),
+              ],
             ),
-            const SizedBox(height: 16.0),
-            const TextField(
-              obscureText: true,
-              decoration: InputDecoration(
-                labelText: 'Password',
-              ),
-            ),
-            const SizedBox(height: 24.0),
-            ElevatedButton(
-              onPressed: () => Navigator.push(context, MaterialPageRoute(builder: (context) => const DashboardPage())),
-              child: const Text('Login'),
-            ),
-          ],
-        ),
-      ),
-    );
+          ),
+        ));
   }
 }
